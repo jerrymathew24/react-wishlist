5acd11f5a678efdeae3eea5d44b22c2a4bfb9efd
Jerry Mathew <jer2ymatheww@gmail.com>
2025-04-06 23:21:20 +0530

    remove wish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { v4 as uuid } from 'uuid';
 
 function App() {
 
-  const [wish,setWish] = useState()
+  const [wish,setWish] = useState('')
   const [wishlist,setWishlist] = useState([])
 
   const onInputChange = (event) => {
@@ -12,10 +12,22 @@ function App() {
   }
 
   const onAddClick = () => {
-    setWishlist([...wishlist, {id:uuid(), wish:wish}])
-    console.log(wishlist);
-    
+    setWishlist([...wishlist, {id:uuid(), wish:wish, isCompleted:false}])
+    setWish('')
   }
+
+  const onDeleteClick = (id) => {
+    const filteredWishlist = wishlist.filter(item => item.id !== id)
+    setWishlist(filteredWishlist)
+  }
+
+  const onInputChekboxChange = (id) => {
+    const updatedWishlist = wishlist.map(item => item.id === id ? {...item, isCompleted:!item.isCompleted} : item)
+    setWishlist(updatedWishlist)
+  }
+
+  
+  console.log(wishlist);
 
   return (
     <>
@@ -29,8 +41,14 @@ function App() {
       <div>
         <h2>My Wishlist</h2>
         <ul>
-          {wishlist.map(item => (
-            <li key={item.id}>{item.wish}</li>
+          {wishlist && wishlist.length > 0 && wishlist.map(item => (
+            <div key={item.id}>
+              <label>
+                <input onChange={()=>onInputChekboxChange(item.id)} type='checkbox' />
+                <span className={ item.isCompleted ? 'strike-through' : '' }>{item.wish}</span>
+              </label>
+              <button onClick={()=>onDeleteClick(item.id)}>Delete</button>
+            </div>
           ))}
         </ul>
       </div>
